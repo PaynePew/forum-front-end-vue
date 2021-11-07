7ca92f3c3e34bf9d302641e21478c5ad3903acaa
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-08 02:28:19 +0800

    feat: add signin page

---
+++ src/views/SignIn.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container py-5">
+    <form action="" class="w-100" @submit.prevent.stop="handleSubmit">
+      <div class="text-center mb-4">
+        <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
+      </div>
+      <div class="form-label-group mb-2">
+        <label for="email">email</label>
+        <input
+          v-model="email"
+          name="email"
+          type="email"
+          id="email"
+          class="form-control"
+          placeholder="email"
+          autocomplete="username"
+          required
+          autofocus
+        />
+      </div>
+
+      <div class="form-label-group mb-3">
+        <label for="password">password</label>
+        <input
+          v-model="password"
+          name="password"
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="password"
+          autocomplete="current-password"
+          required
+        />
+      </div>
+      <button class="btn btn-primary btn-lg w-100 mb-3" type="submit">
+        Submit
+      </button>
+
+      <div class="text-center mb-3">
+        <p><router-link to="/signup">Sign Up</router-link></p>
+      </div>
+
+      <p class="mt-5 mb-3 text-muted text-center">&copy; 2021-2022</p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const data = JSON.stringify({
+        email: this.email,
+        password: this.password,
+      });
+      console.log("data", data);
+    },
+  },
+};
+</script>
