d62d6742343ea4ad18f285219de5616f9a2d7c62
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-08 22:29:28 +0800

    feat: add RestaurantDetail template with dummyData

--- src/views/RestaurantShow.vue
+++ src/views/RestaurantShow.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container py-5">
-    <h1>餐廳描述頁</h1>
     <!-- 餐廳資訊頁 RestaurantDetail -->
+    <RestaurantDetail :initial-restaurant="restaurant" />
     <hr />
     <!-- 餐廳評論 RestaurantComments -->
     <!-- 新增評論 CreateComment -->
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+import RestaurantDetail from "../components/RestaurantDetail.vue";
 const dummyData = {
   restaurant: {
     id: 1,
@@ -56,6 +57,9 @@ const dummyData = {
 };
 
 export default {
+  components: {
+    RestaurantDetail,
+  },
   data() {
     return {
       restaurant: {
@@ -92,7 +96,6 @@ export default {
         isFavorited: dummyData.isFavorited,
         isLiked: dummyData.isLiked,
       };
-
       this.restaurantComments = dummyData.restaurant.Comments;
     },
   },
