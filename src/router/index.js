01ec9416818c30e48a76160e39dec997d567efe6
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-08 15:18:53 +0800

    feat: set restaurants routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,49 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import NotFound from "../views/NotFound.vue";
+import SignIn from "../views/SignIn.vue";
+import Restaurants from "../views/Restaurants.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+
+  {
+    path: "/",
+    name: "root",
+    redirect: "/restaurants",
+  },
   {
     path: "/signin",
     name: "sign-in",
-    component: () => import("../views/SignIn.vue"),
+    component: SignIn,
   },
   {
     path: "/signup",
     name: "sing-up",
     component: () => import("../views/SignUp.vue"),
   },
+  {
+    path: "/restaurants",
+    name: "restaurants",
+    component: Restaurants,
+  },
+  {
+    path: "/restaurants/feeds",
+    name: "restaurants-feeds",
+    component: () => import("../views/RestaurantsFeeds.vue"),
+  },
+  {
+    path: "/restaurants/top",
+    name: "users-top",
+    component: () => import("../views/UsersTop.vue"),
+  },
+  {
+    path: "/users/top",
+    name: "users-top",
+    component: () => import("../views/UsersTop.vue"),
+  },
+
   {
     path: "*",
     name: "not-found",
@@ -23,6 +52,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
+  linkExactActiveClass:'active',
   routes,
 });
 
