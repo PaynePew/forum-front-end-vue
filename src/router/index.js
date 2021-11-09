d22b840e432bce376b64a39c21f28f9fc2c64fa0
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-09 16:04:17 +0800

    feat: add User template and route

--- src/router/index.js
+++ src/router/index.js
@@ -53,6 +53,11 @@ const routes = [
     name: "users-top",
     component: () => import("../views/UsersTop.vue"),
   },
+  {
+    path: "/users/:id",
+    name: "user",
+    component: () => import("../views/User.vue"),
+  },
 
   {
     path: "*",
