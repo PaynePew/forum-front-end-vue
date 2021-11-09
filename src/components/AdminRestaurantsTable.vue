b86bfc97863618e0912538fb9eba39975f9dc67c
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-09 17:37:46 +0800

    feat: add deleteRestaurant in AdminTable

--- src/components/AdminRestaurantsTable.vue
+++ src/components/AdminRestaurantsTable.vue
@@ -27,7 +27,13 @@
 
           <a href="#" class="btn btn-link">Edit</a>
 
-          <button type="button" class="btn btn-link">Delete</button>
+          <button
+            @click.stop.prevent="deleteRestaurant(restaurant.id)"
+            type="button"
+            class="btn btn-link"
+          >
+            Delete
+          </button>
         </td>
       </tr>
     </tbody>
@@ -1082,6 +1088,11 @@ export default {
   methods: {
     fetchRestaurants() {
       this.restaurants = dummyData.restaurants;
+    },
+    deleteRestaurant(restaurantId) {
+      this.restaurants = this.restaurants.filter(
+        (_restaurant) => _restaurant.id !== restaurantId
+      );
     },
   },
 };
