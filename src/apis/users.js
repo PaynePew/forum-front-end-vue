2a64347b497ee1893338857025f7578f87b180a3
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-28 00:54:00 +0800

    feat: add getUsers API

--- src/apis/users.js
+++ src/apis/users.js
@@ -2,6 +2,16 @@ import { apiHelper } from "../utils/helpers";
 const getToken = () => localStorage.getItem("token");
 
 export default {
+  getCurrentUser() {
+    return apiHelper.get(`/get_current_user`, {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
+  },
+  get({ userId }) {
+    return apiHelper.get(`/users/${userId}`, {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
+  },
   addFavorite({ restaurantId }) {
     return apiHelper.post(`/favorite/${restaurantId}`, null, {
       headers: { Authorization: `Bearer ${getToken()}` },
@@ -22,19 +32,19 @@ export default {
       headers: { Authorization: `Bearer ${getToken()}` },
     });
   },
-  getTopUsers () {
-    return apiHelper.get('/users/top', {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+  getTopUsers() {
+    return apiHelper.get("/users/top", {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
   },
-  addFollowing ({ userId }) {
+  addFollowing({ userId }) {
     return apiHelper.post(`/following/${userId}`, null, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
   },
-  deleteFollowing ({ userId }) {
+  deleteFollowing({ userId }) {
     return apiHelper.delete(`/following/${userId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
-  }
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
+  },
 };
